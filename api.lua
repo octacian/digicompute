24991e35d15ea606488e16e1914807a920a7c510
octacian <enduffy2014@outlook.com>
2016-07-12 11:54:13 -0700

    rework formspecs

--- api.lua
+++ api.lua
@@ -3,11 +3,26 @@
 -- FORMSPECS
 -- normal
 function digiterm.formspec_normal(output, input)
-  return 'size[10,11] textarea[.25,.25;10,10.5;output;;'..output..'] button[0,9.5;10,1;update;update] field[.25,10.75;9,1;input;;'..input..'] button[9,10.5;1,1;submit;submit]'
+  if not output then local output = "" end
+  if not input then local input = "" end
+  local formspec =
+    "size[10,11]"..
+    default.gui_bg_img..
+    "textarea[.25,.25;10,10.5;output;Output:;"..output.."]"..
+    "button[0,9.5;10,1;update;Update Output]"..
+    "field[.25,10.75;9,1;input;;"..input.."]"..
+    "button[8.7,10.43;1.30,1;submit;<enter>]"
+  return formspec
 end
--- refresh
-function digiterm.formspec_operating(startspace)
-  return (startspace and ' ' or '')..digiterm.formspec_normal('${output}', '${input}');
+-- set channel (currently unused)
+function digiterm.formspec_channel(channel)
+  if not channel then local channel = "" end -- use blank channel is none specified
+  local formspec =
+    "size[6,1.7]"..
+    default.gui_bg_img..
+    "field[.25,0.50;6,1;channel;Channel:;"..channel.."]"..
+    "button[4.95,1;1,1;submit;Set]"
+  return formspec
 end
 -- /FORMSPECS
 
